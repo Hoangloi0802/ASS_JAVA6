be56ef839d5c5db1369804b843bdb69ecad9959d
Hoangloi <hoangloi@gmail.com>
2025-03-12 18:31:49 +0700

    lan1

---
+++ src/test/java/ass/java6/ass/AssApplicationTests.java
@@ -0,0 +1,13 @@
+package ass.java6.ass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
