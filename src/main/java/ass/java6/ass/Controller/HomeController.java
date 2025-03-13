7865f9edcad5ac9e740d70592aeee74dd9a143b0
your name <vodinhhoi1@gmail.com>
2025-03-13 15:57:22 +0700

    trang admin

--- src/main/java/ass/java6/ass/Controller/HomeController.java
+++ src/main/java/ass/java6/ass/Controller/HomeController.java
@@ -1,7 +1,32 @@
 package ass.java6.ass.Controller;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping("/admin/account")
+    public String account() {
+        return "admin/accountManage";
+    }
 
+    @GetMapping("/admin/categories")
+    public String category() {
+        return "admin/categoryManage";
+    }
+
+    @GetMapping("/admin/products")
+    public String product() {
+        return "admin/productManage";
+    }
+
+    @GetMapping("/admin/bill")
+    public String bill() {
+        return "admin/billManage";
+    }
+
+    @GetMapping("/admin/statistics")
+    public String sattistics() {
+        return "admin/sattistics";
+    }
 }
