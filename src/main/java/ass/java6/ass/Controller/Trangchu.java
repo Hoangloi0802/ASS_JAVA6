6da4cbf19df781d296873e778f261768324e5ec4
Trannhatlongdev2k5 <trannhatlong.16072005@gmail.com>
2025-03-18 22:29:05 +0700

    Cập nhật Trangchu.java

--- src/main/java/ass/java6/ass/Controller/Trangchu.java
+++ src/main/java/ass/java6/ass/Controller/Trangchu.java
@@ -1,29 +1,13 @@
 package ass.java6.ass.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import ass.java6.ass.Dto.DangkyRequest;
-import ass.java6.ass.Dto.DangnhapRequest;
-import ass.java6.ass.Entity.Account;
-import ass.java6.ass.Service.AccoutService;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
-import jakarta.websocket.server.PathParam;
+import ch.qos.logback.core.model.Model;
 
 
 @Controller
-@RequestMapping("/")
 public class Trangchu {
-    @Autowired
-    private AccoutService accoutService;
     @GetMapping("/")
     public String getMethodName(Model model) {
         return "home/trangchu";
@@ -37,8 +21,8 @@ public class Trangchu {
         return "login/Dangnhap";
     }
     @GetMapping("/Dangky")
-    public String Dangky( ){
-        return "testdulieu/Dangky";
+    public String Dangky(){
+        return "login/Dangky";
     }
-
+    
 }
