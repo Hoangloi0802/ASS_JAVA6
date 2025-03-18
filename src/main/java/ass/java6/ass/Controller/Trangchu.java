b6833182ed4853e8fce0ec714d7296f58255e7cc
Hoangloi <hoangloi@gmail.com>
2025-03-18 21:44:49 +0700

    update be lan 2

--- src/main/java/ass/java6/ass/Controller/Trangchu.java
+++ src/main/java/ass/java6/ass/Controller/Trangchu.java
@@ -4,11 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import ch.qos.logback.core.model.Model;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
-
 
 @Controller
 public class Trangchu {
@@ -16,25 +11,53 @@ public class Trangchu {
     public String getMethodName(Model model) {
         return "home/trangchu";
     }
+
     @GetMapping("/shop")
     public String shop(Model model) {
         return "home/sanpham";
     }
+
     @GetMapping("/Dangnhap")
-    public String dangnhap(){
+    public String dangnhap() {
         return "login/Dangnhap";
     }
+
     @GetMapping("/Dangky")
-    public String Dangky(){
+    public String Dangky() {
         return "login/Dangky";
     }
+
     @GetMapping("/thanhtoan")
     public String thanhtoan() {
         return "home/thanhtoan";
     }
+
     @GetMapping("/chitiet")
     public String chitiet() {
         return "home/chitiet";
     }
-    
+
+    @GetMapping("/giohang")
+    public String getMethodName() {
+        return "home/giohang";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "home/profile";
+    }
+
+    @GetMapping("/donmua")
+    public String donmua() {
+        return "home/donmua";
+    }
+
+    @GetMapping("/donhang")
+    public String donhang() {
+        return "home/donhang";
+    }
+    @GetMapping("/chitietdonhang")
+    public String ctdonhang() {
+        return "home/chitietdonhang";
+    }
 }
