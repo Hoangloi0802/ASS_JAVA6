7e0866c7323897db36fabaee624506677e6ab1d6
Hoangloi <hoangloi@gmail.com>
2025-03-20 16:10:07 +0700

    cap nhat drop

--- src/main/java/ass/java6/ass/Service/AccoutService.java
+++ src/main/java/ass/java6/ass/Service/AccoutService.java
@@ -1,16 +1,14 @@
 package ass.java6.ass.Service;
 
-import org.springframework.boot.context.properties.bind.BindResult;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Optional;
 
 import ass.java6.ass.Dto.DangkyRequest;
 import ass.java6.ass.Entity.Account;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 
 public interface AccoutService {
     Account Dangky( DangkyRequest newaccout );
     Account Dangnhap(String username ,String password);
     Account Xacthuc(HttpSession session ,int otpinput);
+    Optional<Account> findByUsername(String username);
 }
