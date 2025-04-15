1378bfd675b7b4b387eb231ab44958bcb6d73caa
phanhuy250205 <162803292+phanhuy250205@users.noreply.github.com>
2025-04-15 23:48:58 +0700

    config vnpay

--- src/main/java/ass/java6/ass/Config/VNPayConfig.java
+++ src/main/java/ass/java6/ass/Config/VNPayConfig.java
@@ -4,8 +4,5 @@ public class VNPayConfig {
     public static final String VNP_TMNCODE = "Y4W66RMK"; // Thay bằng vnp_TmnCode từ VNPay
     public static final String VNP_HASHSECRET = "63BIGTV38Y3QQNZ7SVFVZC039DPAM3IH"; // Thay bằng vnp_HashSecret từ VNPay
     public static final String VNP_PAYURL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"; // URL Sandbox
-    public static final String VNP_RETURNURL = "https://f2b7c5a1187bfdb183a18455984e60df.serveo.net/api/vnpay/callback"; // Thay bằng URL
-                                                                                                     
-                                                                                                    
-
+    public static final String VNP_RETURNURL = "http://localhost:8080/api/vnpay/callback"; // Thay bằng URL                                                                                                                                                                                                
 }
